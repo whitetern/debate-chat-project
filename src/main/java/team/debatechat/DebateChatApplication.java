f190844d0429a32c86a821b4822db49b117d0616
whitetern <dvlpjh@gmail.com>
2024-07-13 15:43:05 +0900

    first commit

---
+++ src/main/java/team/debatechat/DebateChatApplication.java
@@ -0,0 +1,13 @@
+package team.debatechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebateChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebateChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/debatechat/DebateChatApplicationTests.java
@@ -0,0 +1,13 @@
+package team.debatechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebateChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
